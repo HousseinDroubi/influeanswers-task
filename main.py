151b14932efa79b8991f37af7081935cb0ec9d1b
Houssein Droubi <housseinalialdroubi@gmail.com>
2023-02-03 15:43:38 +0200

    Convert 5 numbers to string

--- main.py
+++ main.py
@@ -4,3 +4,11 @@ import pandas as pd
 
 # Get 5 random numbers
 randomlist = random.sample(range(1, 30), 5)
+
+# Convert numbers to string
+random_list_str = []
+for x in randomlist:
+  if(x>=10):
+    random_list_str.append(str(x))
+  else:
+    random_list_str.append("0"+str(x))
