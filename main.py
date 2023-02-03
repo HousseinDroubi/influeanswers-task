6537d26ddba6422e6ee9d51e452d978d83503b5e
Houssein Droubi <housseinalialdroubi@gmail.com>
2023-02-03 15:45:02 +0200

    Implement a function to download text file

--- main.py
+++ main.py
@@ -12,3 +12,14 @@ for x in randomlist:
     random_list_str.append(str(x))
   else:
     random_list_str.append("0"+str(x))
+
+# The below function is to download text file
+def download_file(day):
+    date = "2014-06-"+day  
+    base_url = "https://chatlogs.planetrdf.com/swig/"+date+".txt"  
+    target_txt_path = "chat-days/"+date+".txt"
+    response = requests.get(base_url)
+    response.raise_for_status() 
+    with open(target_txt_path, "wb") as f:
+        f.write(response.content)
+    print("File downloaded.")
